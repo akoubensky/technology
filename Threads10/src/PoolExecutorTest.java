381f8100d09837427e2b62067a57033d587c5437
akoubensky <akoubensky@gmail.com>
2014-08-13 17:43:36 +0400

    Pool Executor refinement

--- Threads10/src/PoolExecutorTest.java
+++ Threads10/src/PoolExecutorTest.java
@@ -1,23 +1,30 @@
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 
 public class PoolExecutorTest {
-  final static Runnable process = new Runnable() {
-    public void run() {
-      for (int i = 0; i < 4; i++) {
-        System.out.println(Thread.currentThread().getName() + ": " + i);
-      }
-    }
-  };
+	private static class MyProcess implements Runnable {
+		final int number;
+		
+		public MyProcess(int n) { number = n; }
+		
+	    public void run() {
+	      for (int i = 0; i < 3; i++) {
+	        System.out.format("Thread: %s; Process: %d; Iteration: %d%n",
+	        		Thread.currentThread().getName(), number, i);
+	      }
+	    }
+	}
   
   public static void main(String[] args) throws InterruptedException {
     ExecutorService executor = Executors.newFixedThreadPool(3);
     
-    executor.execute(process);
-    executor.execute(process);
-    executor.execute(process);
+    for (int i = 0; i < 25; i++) {
+    	executor.execute(new MyProcess(i));
+    }
     executor.shutdown();
+    executor.awaitTermination(1, TimeUnit.MINUTES);
     
     System.out.println("Все процессы созданы и запущены");
   }

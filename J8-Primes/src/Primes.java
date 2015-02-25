217cb240f83181237ffd916bdfb6f53f7add11a5
Alexander Kubenskiy <akoubensky@gmail.com>
2015-02-25 22:31:00 +0300

    Primes on streams

--- J8-Primes/src/Primes.java
+++ J8-Primes/src/Primes.java
@@ -1,21 +1,42 @@
 import java.util.ArrayList;
 import java.util.List;
 
-
+/**
+ * Генерация списка простых чисел простым перебором
+ * нечетных элементов с проверкой их на простоту.
+ */
 public class Primes {
+	// Количество простых чисел, которое необходимо сгенерировать
 	private int size;
+	
+	// Список простых чисел (вначале пустой)
 	public List<Integer> primes = new ArrayList<>();
 	
+	/**
+	 * Конструктор запкскает генератор простых чисел.
+	 * @param size	Количество простых чисел, которые нужно сгенерировать
+	 */
 	public Primes(int size) {
 		this.size = size;
 		generatePrimes();
 	}
 	
+	/**
+	 * Добавляет в список простых чисел еще некоторое количество простых.
+	 * @param n	Сколько еще нужно добавить простых чисел.
+	 */
 	public void add(int n) {
+		if (n <= 0) throw new IllegalArgumentException(
+				"Добавить можно только положительное число простых");
 		size += n;
 		generatePrimes();
 	}
 	
+	/**
+	 * Перебирает нечетные числа, начиная с последнего сгенерированного числа,
+	 * и проверяет их на простоту. Первое встретившееся простое число добавляется
+	 * в список простых чисел.
+	 */
 	private void generatePrimes() {
 		int s;
 		while ((s = primes.size()) < size) {
@@ -29,16 +50,28 @@ public class Primes {
 		}
 	}
 	
+	/**
+	 * Проверка числа на простоту.
+	 * Используется уже сгенерированный список простых чисел.
+	 * @param n	Проверяемое число.
+	 * @return
+	 */
 	private boolean isPrime(int n) {
 		for (Integer p : primes) {
+			if (p * p > n) return true;
 			if (n % p == 0) return false;
 		}
 		return true;
 	}
 
 	public static void main(String[] args) {
-		List<Integer> primes = new Primes(50001).primes;
-		System.out.println(primes.get(50000));
+		// Генерируем 100001 простых чисел.
+		List<Integer> primes = new Primes(100001).primes;
+		// В качестве проверки печатаем первые 30 простых чисел.
+		primes.stream().limit(30).forEach(x -> System.out.print(" " + x));
+		System.out.println();
+		// Теперь печатаем последнее найденное простое число.
+		System.out.println(primes.get(100000));
 	}
 
 }
---
+++ J8-Primes/src/SplitPrimes.java
@@ -0,0 +1,64 @@
+import java.util.Arrays;
+import java.util.Spliterator;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+/**
+ * Генерация ограниченного потока первых простых чисел методом "решета Эратосфена".
+ */
+public class SplitPrimes {
+	/**
+	 * "Ячейка" для хранения очередного простого числа.
+	 */
+	private static class Buffer {
+		private Integer buffer;
+		public Integer get() { return buffer; }
+		public void set(Integer n) { buffer = n; }
+	}
+	
+	private Buffer buffer = new Buffer();
+	
+	/**
+	 * Сохраняет первое число из потока в накопителе и генерирует новый
+	 * поток, отбрасывая первое число и фильтруя оставшиеся таким образом,
+	 * чтобы в нем не осталось чисел, кратных первому.
+	 * @param s	Исходный поток (первое число в нем - простое)
+	 * @return	Отфильтрованный поток без первого элемента.
+	 */
+	private Stream<Integer> sieve(Stream<Integer> s, Stream.Builder<Integer> builder) {
+		Spliterator<Integer> spliterator = s.spliterator();
+		spliterator.tryAdvance(buffer::set);
+		Integer next = buffer.get();
+		builder.add(next);
+		return StreamSupport.stream(spliterator, false).filter(x -> x % next != 0);
+	}
+	
+	/**
+	 * Генератор заданного количества простых чисел.
+	 * @param n	Количество генерируемых простых чисел.
+	 * @return	Поток простых чисел.
+	 */
+	public Stream<Integer> primes(int n) {
+		Stream.Builder<Integer> builder = Stream.builder();
+		Stream<Integer> s = Stream.iterate(2, x -> x+1);
+		while (n-- > 0) {
+			s = sieve(s, builder);
+		}
+		return builder.build();
+	}
+	
+	/**
+	 * Генерирует поток из заданного количества простых чисел и выдает их в виде массива.
+	 * @param n
+	 * @return
+	 */
+	public Integer[] getPrimes(int n) {
+		return primes(n).toArray(Integer[]::new);
+	}
+	
+	public static void main(String[] args) {
+		SplitPrimes s = new SplitPrimes();
+		System.out.println(Arrays.toString(s.getPrimes(20)));
+		System.out.println(s.getPrimes(10000)[9999]);
+	}
+}
